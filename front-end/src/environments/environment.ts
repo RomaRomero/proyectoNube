42bdc0b84036a7c91f458ba65d5d2c0115568467
Bryam <donalk100@gmail.com>
2025-06-15 16:32:09 -0500

    cambios para pull

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,12 +3,12 @@ export const environment = {
   apiUrl: 'https://api.staging.miproyecto.com/api',
   apiUrlMicroservice: 'http://localhost:7000/api/notify/push',
   firebaseConfig:{
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    vapidKey: ""
+    apiKey: "AIzaSyCzSRmhQ-5Y8TkMaJpXB3q-YcUYFw06nDA",
+    authDomain: "push-fit.firebaseapp.com",
+    projectId: "push-fit",
+    storageBucket: "push-fit.firebasestorage.app",
+    messagingSenderId: "423673148218",
+    appId: "1:423673148218:web:4d93c9b85b28c19b5c9603",
+    vapidKey: "BG2EPMeji_K-BZdvLPTF_o8rHT4tXyLGhroeIv8ek95rn6uB-CCjdHOBnju_d8jxSdCfyN_EXx9pd0Fpge1oQDo"
   }
 };
--- front-end/src/firebase-messaging-sw.js
+++ front-end/src/firebase-messaging-sw.js
@@ -1,12 +1,12 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyCzSRmhQ-5Y8TkMaJpXB3q-YcUYFw06nDA",
+    authDomain: "push-fit.firebaseapp.com",
+    projectId: "push-fit",
+    storageBucket: "push-fit.firebasestorage.app",
+    messagingSenderId: "423673148218",
+    appId: "1:423673148218:web:4d93c9b85b28c19b5c9603",
 });
 
 // Inicializa Firebase
