80998e9b328bb4a27d876f3edefeacc604453625
Oscar Zurita <oscar.zurita.est@tecazuay.edu.ec>
2025-06-15 17:37:46 -0500

    avance oscar

--- front-end/src/app/features/auth/pages/dashboard/dashboard.component.ts
+++ front-end/src/app/features/auth/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {jwtDecode} from 'jwt-decode';
+import { AuthService } from '../../../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  imports: [FormsModule,CommonModule,HttpClientModule],
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  rol: string | null = null;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const token = this.authService.getToken();
+    if (token) {
+      const decoded: any = jwtDecode(token);
+      this.rol = decoded.rol || null;
+    }
+  }
 }
